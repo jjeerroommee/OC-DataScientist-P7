e484addec1ec7a0ba408f269aab0f684970c95ba
JOURDAN Jerome DTSI/DTR <DTCB8049@orange.com>
2024-05-17 15:46:15 +0200

    publication de l'API version Cloud Azure

--- .ipynb_checkpoints/app-checkpoint.py
+++ .ipynb_checkpoints/app-checkpoint.py
@@ -1,6 +1,7 @@
 import joblib
 import os
-from flask import Flask, render_template, jsonify
+import pandas as pd
+from flask import Flask, render_template, jsonify, request
 
 
 app = Flask(__name__)
@@ -12,16 +13,28 @@ def home():
     return render_template('index.html')
 
 
-
-@app.route('/predict/')
+@app.route('/predict/', methods=['POST'])
 def classif():
-    #print("hello predict")
     # Loads a model previously saved 
     model = joblib.load(os.path.join('models', 'lr-pipe_8feat.joblib'))
         
+    #getting an array of features from the post request's body
+    data_json = request.get_json()
+    data = pd.DataFrame(data_json['inputs'])
+
+    
+    #creating a response object
+    proba_echec = model.predict_proba(data)[0][1]
+
+    if proba_echec > 0.5 :
+        classe = 'refusé'
+    else :
+        classe = 'accepté'
+    
+    #returning the response object as json
     return jsonify({
-      'classe': 'crédit accordé', 
-      'probabilité de défaut de paiement': 0.2
+        'classe': classe, 
+        'proba_echec': proba_echec
     })
 
 
--- project/tdb_streamlit/dashboard.py
+++ project/tdb_streamlit/dashboard.py
@@ -1,4 +1,3 @@
-import pandas as pd
 import streamlit as st
 import requests
 
@@ -9,9 +8,11 @@ def request_prediction(data):
     response = requests.request(
         method='POST',
         headers={"Content-Type": "application/json"},
-        url='http://localhost:5000/predict',
+        
+        # url='http://localhost:5000/predict', # when using a local flask server
+        url='https://oc-datascientist-p7-lgbm.azurewebsites.net/predict/', # when using an Azure clouded server
         json=data_json)
-
+    
     if response.status_code != 200 :
         raise Exception(
             "Request failed with status {}, {}".format(response.status_code, response.text))
@@ -21,39 +22,37 @@ def request_prediction(data):
 
 def main():
     st.title("Test de l'API de prédiction de solvabilité d'un client bancaire")
+    data={}
+        
+    data['EXT_SOURCE_3'] = [st.number_input('ext_source_3 (décimal entre 0 et 1)',
+                                   min_value=0., max_value=1.0, value=0.50, step=0.01)]
 
-    ext_source_3 = st.number_input('ext_source_3 (décimal entre 0 et 1)',
-                                   min_value=0., max_value=1.0, value=0.50, step=0.01)
+    data['EXT_SOURCE_2'] = [st.number_input('ext_source_2 (décimal entre 0 et 1)',
+                              min_value=0., max_value=1.0, value=0.49, step=0.01)]
 
-    ext_source_2 = st.number_input('ext_source_2 (décimal entre 0 et 1)',
-                              min_value=0., max_value=1.0, value=0.49, step=0.01)
+    data['PAYMENT_RATE'] = [st.number_input('payment_rate (décimal entre 0 et 1)',
+                                   min_value=0.01, max_value=.13, value=0.05, step=0.01)]
 
-    payment_rate = st.number_input('payment_rate (décimal entre 0 et 1)',
-                                   min_value=0.01, max_value=.13, value=0.05, step=0.01)
+    data['EXT_SOURCE_1'] = [st.number_input('ext_source_1 (décimal entre 0 et 1)',
+                                   min_value=0., max_value=1.0, value=0.50, step=0.01)]
 
-    ext_source_1 = st.number_input('ext_source_1 (décimal entre 0 et 1)',
-                                   min_value=0., max_value=1.0, value=0.50, step=0.01)
+    data['NAME_FAMILY_STATUS_Married'] = [st.number_input('name_family_status_married (booléen)',
+                                 min_value=0, max_value=1, value=1, step=1)]
 
-    name_family_status_married = st.number_input('name_family_status_married (booléen)',
-                                 min_value=0, max_value=1, value=1, step=1)
+    data['CODE_GENDER_F'] = [st.number_input('code_gender_f (booléen)',
+                                    min_value=0, max_value=1, value=1, step=1)]
 
-    code_gender_f = st.number_input('code_gender_f (booléen)',
-                                    min_value=0, max_value=1, value=1, step=1)
+    data['AMT_ANNUITY'] = [st.number_input('amt_annuity (entier avec pas = 1000)',
+                                  min_value=1000, max_value=130000, value=27000, step=1000)]
 
-    amt_annuity = st.number_input('amt_annuity (entier avec pas = 1000)',
-                                  min_value=1000, max_value=130000, value=27000, step=1000)
-
-    approved_cnd_payment_mean = st.number_input('approved_cnd_payment_mean (entier avec pas = 5)',
-                                                min_value=0, max_value=100, value=14, step=5)
+    data['APPROVED_CNT_PAYMENT_MEAN'] = [st.number_input('approved_cnt_payment_mean (entier avec pas = 1)',
+                                                min_value=0, max_value=100, value=14, step=1)]
 
     predict_btn = st.button('Prédire')
-    if predict_btn:
-        data = [[ext_source_3, ext_source_2, payment_rate, ext_source_1,
-                 name_family_status_married, code_gender_f, amt_annuity, approved_cnd_payment_mean]]
-        
+    if predict_btn :
         pred = request_prediction(data)
-        st.write(
-            'Variable 1 = {:.2f}'.format(data[0]))
+        st.write('Classe (crédit accepté ou refusé) : ', pred['classe'])
+        st.write('Probabilité de défaut du client (0 : client  solvable): ', pred['proba_echec'])
 
 
 if __name__ == '__main__':
